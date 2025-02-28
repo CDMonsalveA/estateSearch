07448e5b187def1b3b1d31028c44d0c5017c91b0
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-02-27 22:26:22 -0500

    refactor test cases in TestRightmove to improve location handling and streamline sample data

--- tests/test_search_uk.py
+++ tests/test_search_uk.py
@@ -1,4 +1,3 @@
-import asyncio
 import unittest
 from random import shuffle
 
@@ -6,17 +5,46 @@ import requests
 
 from estatesearch import Rightmove
 
+unittest.TestLoader.sortTestMethodsUsing = None
+
+samples = [
+    # POSTCODE
+    ("SY3 9EB", "POSTCODE", "4203018"),
+    ("sy3-9eb", "POSTCODE", "4203018"),
+    ("W1A 1AA", "POSTCODE", "912358"),
+    ("w1a-1aa", "POSTCODE", "912358"),
+    ("B1 1AA", "POSTCODE", "4183557"),
+    ("CR0 5NS", "POSTCODE", "184365"),
+    ("CR0-5NS", "POSTCODE", "184365"),
+    # AREA
+    ("london", "REGION", "93917"),
+    ("birmingham", "REGION", "162"),
+    ("manchester", "REGION", "904"),
+    ("liverpool", "REGION", "813"),
+    ("leeds", "REGION", "787"),
+    ("bristol", "REGION", "219"),
+    ("edinburgh", "REGION", "475"),
+    ("glasgow", "REGION", "550"),
+    ("cardiff", "REGION", "281"),
+    ("swansea", "REGION", "1305"),
+    # TRAIN STATION
+    ("paddington", "REGION", "70403"),
+]
+
 
 class TestRightmove(unittest.TestCase):
     """
     Test the Rightmove class
     """
 
+    # test for warnings with no location
+    # test for warnings with no buy_or_rent
+    # test for warnings with no location and no buy_or_rent
     def test_connection(self):
         """
         Test the connection to the Rightmove website.
         """
-        rightmove = Rightmove()
+        rightmove = Rightmove(location="london")
         status_code = requests.get(rightmove.url).status_code
         self.assertEqual(status_code, 200)
 
@@ -24,7 +52,7 @@ class TestRightmove(unittest.TestCase):
         """
         Test the connection to the Rightmove API.
         """
-        rightmove = Rightmove()
+        rightmove = Rightmove(location="london")
         status_code = requests.get(rightmove.api_url).status_code
         self.assertEqual(status_code, 200)
 
@@ -32,7 +60,7 @@ class TestRightmove(unittest.TestCase):
         """
         Evaluate if the search bar is present on the website.
         """
-        rightmove = Rightmove()
+        rightmove = Rightmove(location="london")
         website_content = requests.get(rightmove.url).text
         self.assertIn("ta_searchInput", website_content)
 
@@ -40,7 +68,7 @@ class TestRightmove(unittest.TestCase):
         """
         Evaluate if the 'for sale' button is present on the website.
         """
-        rightmove = Rightmove()
+        rightmove = Rightmove(location="london")
         website_content = requests.get(rightmove.url).text
         self.assertIn("dsrm_button_content", website_content)
 
@@ -48,27 +76,7 @@ class TestRightmove(unittest.TestCase):
         """
         Test the connection to the Rightmove house prices website.
         """
-        places = [
-            "london",
-            "me2",
-            "paddington",
-            "w1",
-            "sy3-9eb",
-            "birmingham",
-            "manchester",
-            "liverpool",
-            "leeds",
-            "bristol",
-            "edinburgh",
-            "glasgow",
-            "cardiff",
-            "swansea",
-            "ME2-4JZ",
-            "W1A-1AA",
-            "SY3-9EB",
-            "B1-1AA",
-            "M1-1AA",
-        ]
+        places = list(set([place[0] for place in samples]))
         shuffle(places)
 
         for place in places:
@@ -80,31 +88,8 @@ class TestRightmove(unittest.TestCase):
         """
         Test the get_location_id method.
         """
-        samples = [
-            # POSTCODE
-            ("SY3 9EB", "POSTCODE", "4203018"),
-            ("sy3-9eb", "POSTCODE", "4203018"),
-            ("W1A 1AA", "POSTCODE", "912358"),
-            ("w1a-1aa", "POSTCODE", "912358"),
-            ("B1 1AA", "POSTCODE", "4183557"),
-            ("CR0 5NS", "POSTCODE", "184365"),
-            ("CR0-5NS", "POSTCODE", "184365"),
-            # AREA
-            ("london", "REGION", "93917"),
-            ("birmingham", "REGION", "162"),
-            ("manchester", "REGION", "904"),
-            ("liverpool", "REGION", "813"),
-            ("leeds", "REGION", "787"),
-            ("bristol", "REGION", "219"),
-            ("edinburgh", "REGION", "475"),
-            ("glasgow", "REGION", "550"),
-            ("cardiff", "REGION", "281"),
-            ("swansea", "REGION", "1305"),
-            # TRAIN STATION
-            ("paddington", "REGION", "70403"),
-        ]
-        shuffle(samples)
-
-        for location, location_type, location_id in samples:
+        locations = samples
+        shuffle(locations)
+        for location, location_type, location_id in locations:
             rightmove = Rightmove(location=location)
             self.assertEqual(rightmove.get_location_id(), (location_type, location_id))
