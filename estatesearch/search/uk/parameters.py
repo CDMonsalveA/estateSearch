f4c45646170cb501ed720da5c6f79282ac81ef5a
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-03-06 22:11:56 -0500

    Refactor RightmoveParameters class to propertiesParameters with detailed attributes for property data

--- estatesearch/search/uk/parameters.py
+++ estatesearch/search/uk/parameters.py
@@ -5,16 +5,63 @@ Definition of the parameters to be obtained from the web portals.
 from typing import NamedTuple
 
 
-class RightmoveParameters(NamedTuple):
-    buy_or_rent: str
+class propertiesParameters(NamedTuple):
+    """
+    Parameters for the properties to be obtained from the web portals.
+
+    The Literal information from Rightmove search:
+    'id', 'bedrooms', 'bathrooms', 'numberOfImages', 'numberOfFloorplans', 'numberOfVirtualTours', 'summary', 'displayAddress', 'countryCode', 'location', 'propertyImages', 'propertySubType', 'listingUpdate', 'premiumListing', 'featuredProperty', 'price', 'customer', 'distance', 'transactionType', 'productLabel', 'commercial', 'development', 'residential', 'students', 'auction', 'feesApply', 'feesApplyText', 'displaySize', 'showOnMap', 'propertyUrl', 'contactUrl', 'staticMapUrl', 'channel', 'firstVisibleDate', 'keywords', 'keywordMatchType', 'saved', 'hidden', 'onlineViewingsAvailable', 'lozengeModel', 'hasBrandPlus', 'displayStatus', 'enquiredTimestamp', 'enquiryAddedTimestamp', 'enquiryCalledTimestamp', 'heading', 'addedOrReduced', 'formattedBranchName', 'formattedDistance', 'propertyTypeFullDescription', 'isRecent', 'enhancedListing'
+    """
+
+    id: str
+    bedrooms: str
+    bathrooms: str
+    numberOfImages: str
+    numberOfFloorplans: str
+    numberOfVirtualTours: str
+    summary: str
+    displayAddress: str
+    countryCode: str
     location: str
-    radius: float
-    min_price: float
-    max_price: float
-    min_bedrooms: int
-    max_bedrooms: int
-    property_types: list
-    max_days_since_added: int
-    include_sstc: bool
-    must_have: list
-    dont_show: list
+    propertyImages: str
+    propertySubType: str
+    listingUpdate: str
+    premiumListing: str
+    featuredProperty: str
+    price: str
+    customer: str
+    distance: str
+    transactionType: str
+    productLabel: str
+    commercial: str
+    development: str
+    residential: str
+    students: str
+    auction: str
+    feesApply: str
+    feesApplyText: str
+    displaySize: str
+    showOnMap: str
+    propertyUrl: str
+    contactUrl: str
+    staticMapUrl: str
+    channel: str
+    firstVisibleDate: str
+    keywords: str
+    keywordMatchType: str
+    saved: str
+    hidden: str
+    onlineViewingsAvailable: str
+    lozengeModel: str
+    hasBrandPlus: str
+    displayStatus: str
+    enquiredTimestamp: str
+    enquiryAddedTimestamp: str
+    enquiryCalledTimestamp: str
+    heading: str
+    addedOrReduced: str
+    formattedBranchName: str
+    formattedDistance: str
+    propertyTypeFullDescription: str
+    isRecent: str
+    enhancedListing: str
