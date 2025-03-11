a2c233895b620630f6fff2cb661b70a6970c7dd6
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-03-10 21:53:18 -0500

    Add AsyncClient configuration with HTTP/2 support and custom headers in rightmove.py

--- estatesearch/search/uk/rightmove.py
+++ estatesearch/search/uk/rightmove.py
@@ -18,7 +18,19 @@ from typing import List
 
 import requests
 from httpx import AsyncClient, Response
+from parsel import Selector
 
+client = AsyncClient(
+    headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,lt;q=0.8,et;q=0.7,de;q=0.6",
+    },
+    follow_redirects=True,
+    http2=True,  # enable http2 to reduce block chance
+    timeout=30,
+)
 
 class Rightmove:
     """
