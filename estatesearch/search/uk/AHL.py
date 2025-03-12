8b57db4df7d68fb0a91db9967a4d93c86e930403
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-03-11 22:45:45 -0500

    Add AHL module for property search on Auction House London website

---
+++ estatesearch/search/uk/AHL.py
@@ -0,0 +1,11 @@
+"""
+This module contains the AHL (Auction House London) class
+which is used to search for properties on
+the Auction House London main website.
+
+auctionhouselondon.co.uk/ is the website of the biggest auction house in London.
+Allows users to search for good deals in properties for sale in the UK.
+
+check out the website at: https://auctionhouselondon.co.uk/
+and the robots.txt file at: https://auctionhouselondon.co.uk/robots.txt
+"""
