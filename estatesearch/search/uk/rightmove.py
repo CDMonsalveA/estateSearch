cae870bae59aa58a2bde3cd8de15c5a197869d69
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-03-14 23:35:18 -0500

    Remove commented-out code and improve clarity in Rightmove search implementation

--- estatesearch/search/uk/rightmove.py
+++ estatesearch/search/uk/rightmove.py
@@ -13,9 +13,6 @@ and the robots.txt file at: https://www.rightmove.co.uk/robots.txt
 
 import asyncio
 import json
-import random
-import re
-import time
 from typing import List
 
 import requests
@@ -258,7 +255,6 @@ class Rightmove:
         total_results = int(
             json.loads(response.text)["resultCount"].replace(",", "")
         )
-        # print(f"total_results: {total_results}")
         properties = json.loads(response.text)["properties"]
 
         if total_results > self.properties_per_page:
@@ -268,35 +264,14 @@ class Rightmove:
                 total_results,
                 self.properties_per_page,
             ):
-                # print("i", i)
-                # stop = False
+
                 lower_limit = i
                 upper_limit = self.properties_per_page
                 if i > API_LIMIT:
                     break
-                # if i + self.properties_per_page >= API_LIMIT:
-                #     gap_until_limit = API_LIMIT - i
-                #     lower_limit = i
-                #     upper_limit = gap_until_limit
-                #     print("gap_until_limit", gap_until_limit)
-                # if i >= API_LIMIT:
-                #     lower_limit = API_LIMIT
-                #     upper_limit = self.properties_per_page
-                #     print("i", i)
-                #     stop = True
                 url = f"{self.search_url_api}&index={lower_limit}&numberOfPropertiesPerPage={upper_limit}"
-                # print(url)
-                # print(f"getting properties {lower_limit} to {lower_limit + upper_limit}")
                 response = requests.get(url)
-                # print(f"# of properties: {len(json.loads(response.text)['properties'])}")
                 properties += json.loads(response.text)["properties"]
-                # if stop:
-                #     break
-        # Check number of duplicates in id
-        # ids = [property["id"] for property in properties]
-        # duplicates = len(ids) - len(set(ids))
-        # if duplicates > 0:
-        #     print(f"Warning: {duplicates} duplicates found in the search results.")
         if total_results > len(properties):
             print(
                 f"\nWarning: {total_results} total results but it was only possible to get {len(properties)} results."
@@ -315,7 +290,6 @@ class Rightmove:
             urls.append(
                 f"https://www.rightmove.co.uk{property_data['propertyUrl']}"
             )
-        # get unique urls
         urls = list(set(urls))
         return urls
 
