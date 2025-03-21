1ee5186c956ffb16376802e63e5b928d86c503ee
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-03-21 12:55:31 -0500

    Add main module for Estate Search application

---
+++ estatesearch/main.py
@@ -0,0 +1,18 @@
+"""
+Main Module for the Estate Search application.
+This module controls the flow of the application, including loading configuration,
+searching for properties, and saving results.
+It serves as the entry point for the application and coordinates the various components.
+"""
+
+if __name__ == "__main__":
+
+    # Search / Fetch the data
+
+    # Download / Save the data
+    # Process / Clean, Filter, Transform
+    # Analyze / Visualize, Report, Statistics, Machine Learning, etc.
+    # Present / UI, Web, API, etc.
+    # Select / Filter, Sort, Rank, etc.
+    # Export / Save, Share, Publish, etc.
+    pass
