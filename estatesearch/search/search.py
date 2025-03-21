bd516a1916027fcf040cc19a8a6c961fa3cf15a7
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-03-21 12:56:06 -0500

    Add search manager for estate search application

---
+++ estatesearch/search/search.py
@@ -0,0 +1,15 @@
+"""Search manager for the estate search application."""
+
+
+def SearchManager():
+    """
+    Search manager for the estate search application.
+
+    This function manages the search process
+    """
+    # Make a new search
+
+    # Add addional information to the search [e.g. country, dateOfSearch, etc.]
+
+    # Return the search results as a JSON object
+    pass
