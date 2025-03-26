42a29f14961a982c99bd4f951b6434a0b9f14bc3
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-03-26 00:26:35 -0500

    Implement search functionality and save results to JSON file

--- estatesearch/main.py
+++ estatesearch/main.py
@@ -4,7 +4,9 @@ This module controls the flow of the application, including loading configuratio
 searching for properties, and saving results.
 It serves as the entry point for the application and coordinates the various components.
 """
-
+import json
+from estatesearch.search.searchConfig import SearchParams
+from estatesearch.search.search import SearchManager
 
 def main():
     """
@@ -13,7 +15,15 @@ def main():
     including loading configuration, searching for properties, and saving results.
     """
     # Search / Fetch the data
+    params = SearchParams(location="London", buy_rent="buy", max_days_since_added=1,
+                          max_price=500000,)
+    search_results = SearchManager(params)
+
     # Download / Save the data
+    # Save the search results to a JSON file
+    json_results = json.loads(search_results)
+    with open("search_results.json", "w") as f:
+        json.dump(json_results, f, indent=4)
     # Process / Clean, Filter, Transform
     # Analyze / Visualize, Report, Statistics, Machine Learning, etc.
     # Present / UI, Web, API, etc.
