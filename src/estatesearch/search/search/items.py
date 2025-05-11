f5a722e9df05c107edd5c15897b09a692a7679d1
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-05-11 17:12:55 -0500

    Refactor items.py: Expand PropertyItem and add new item classes for enhanced property data modeling

--- src/estatesearch/search/search/items.py
+++ src/estatesearch/search/search/items.py
@@ -28,19 +28,49 @@ class PropertyItem(scrapy.Item):
     price = scrapy.Field()
     price_frequency = scrapy.Field()
     price_currencyCode = scrapy.Field()
-    contactTelephone = scrapy.Field() # customer > contactTelephone
-    contact_branchDisplayName = scrapy.Field() # customer > contactBranchDisplayName
-    commercial = scrapy.Field() # commercial
-    development = scrapy.Field() # development
-    residential = scrapy.Field() # residential
-    students = scrapy.Field() # students
-    auction = scrapy.Field() # auction
-    feesApply = scrapy.Field() # feesApply
-    displaySize = scrapy.Field() # displaySize
+    contactTelephone = scrapy.Field()  # customer > contactTelephone
+    contact_branchDisplayName = (
+        scrapy.Field()
+    )  # customer > contactBranchDisplayName
+    commercial = scrapy.Field()  # commercial
+    development = scrapy.Field()  # development
+    residential = scrapy.Field()  # residential
+    students = scrapy.Field()  # students
+    auction = scrapy.Field()  # auction
+    feesApply = scrapy.Field()  # feesApply
+    displaySize = scrapy.Field()  # displaySize
     propertyUrl = scrapy.Field()  # propertyUrl
     firstVisibleDate = scrapy.Field()  # firstVisibleDate
     propertyTypeFullDescription = scrapy.Field()  # propertyTypeFullDescription
     isRecent = scrapy.Field()  # isRecent
+
+    # ----- FROM pageMODEL propertyData Per each property page-----
+    status = scrapy.Field()  # .status .published
+    price_displayPriceQualifier = scrapy.Field()
+    brochure_url = scrapy.Field()  # brochures > url -- first
+    epcGRAPH = scrapy.Field()  # epcGraph
+    feesApply = scrapy.Field()  # feesApply
+    lettings = scrapy.Field()  # lettings
+    tenure_type = scrapy.Field()  # tenure > ternureType
+    tenure_years = scrapy.Field()  # tenure > yearsRemainingOnLease
+    propertySubType = scrapy.Field()  # propertySubType
+
+    # ----- FROM pageMODEL analyticsInfo Per each property page-----
+    displayAddress = scrapy.Field()  # analyticsBranch .displayAddress
+    postcode = scrapy.Field()  # analyticsProperty .postcode
+    added = scrapy.Field()  # analyticsProperty .added
+    auctionOnly = scrapy.Field()  # analyticsProperty .auctionOnly
+    businessForSale = scrapy.Field()  # analyticsProperty .businessForSale
+    letAgreed = scrapy.Field()  # analyticsProperty .letAgreed
+    lettingType = scrapy.Field()  # analyticsProperty .lettingType
+    ownership = scrapy.Field()  # analyticsProperty .ownership
+    preOwned = scrapy.Field()  # analyticsProperty .preOwned
+    price_pageModel = scrapy.Field()  # analyticsProperty .price
+    priceQualifier = scrapy.Field()  # analyticsProperty .priceQualifier
+    propertyType = scrapy.Field()  # analyticsProperty .propertyType
+    propertySubType = scrapy.Field()  # analyticsProperty .propertySubType
+    retirement = scrapy.Field()  # analyticsProperty .retirement
+    soldSTC = scrapy.Field()  # analyticsProperty .soldSTC
 
 
 class PropertyImageItem(scrapy.Item):
@@ -65,4 +95,68 @@ class PropertyLocationItem(scrapy.Item):
     regionName = scrapy.Field()
     districtName = scrapy.Field()
     subDistrictName = scrapy.Field()
+
+    # FROM pageMODEL propertyData Per each property page
+    pinType = scrapy.Field()  # location > pinType
     pass
+
+
+class PropertyRoomItem(scrapy.Item):
+    # FROM pageMODEL propertyData Per each property page
+    id = scrapy.Field()
+    name = scrapy.Field()  # rooms > name
+    description = scrapy.Field()  # rooms > description
+    width = scrapy.Field()  # rooms > width
+    length = scrapy.Field()  # rooms > length
+    unit = scrapy.Field()  # rooms > unit
+    dimension = scrapy.Field()  # rooms > dimension
+
+
+class PropertyNearestAirportItem(scrapy.Item):
+    # FROM pageMODEL propertyData Per each property page
+    id = scrapy.Field()
+    name = scrapy.Field()  # nearestAirports > name
+    distance = scrapy.Field()  # nearestAirports > distance
+    unit = scrapy.Field()  # nearestAirports > unit
+    pass
+
+
+class PropertyNearestStationItem(scrapy.Item):
+    # FROM pageMODEL propertyData Per each property page
+    id = scrapy.Field()
+    name = scrapy.Field()  # nearestStations > name
+    distance = scrapy.Field()  # nearestStations > distance
+    unit = scrapy.Field()  # nearestStations > unit
+    pass
+
+
+class PropertyLivingCostItem(scrapy.Item):
+    # FROM pageMODEL propertyData Per each property page
+    id = scrapy.Field()
+    councilTaxExempt = scrapy.Field()  # livingCosts > councilTaxExempt
+    councilTaxIncluded = scrapy.Field()  # livingCosts > councilTaxIncluded
+    annualGroundRent = scrapy.Field()  # livingCosts > annualGroundRent
+    groundRentReviewPeriodInYears = (
+        scrapy.Field()
+    )  # livingCosts > groundRentReviewPeriodInYears
+    groundRentPercentageIncrease = (
+        scrapy.Field()
+    )  # livingCosts > groundRentPercentageIncrease
+    annualServiceCharge = scrapy.Field()  # livingCosts > annualServiceCharge
+    councilTaxBand = scrapy.Field()  # livingCosts > councilTaxBand
+    domesticRates = scrapy.Field()  # livingCosts > domesticRates
+
+
+class PropertyFeatureItem(scrapy.Item):
+    # FROM pageMODEL propertyData Per each property page
+    id = scrapy.Field()
+    electricity = scrapy.Field()  # features > electricity
+    broadband = scrapy.Field()  # features > broadband
+    water = scrapy.Field()  # features > water
+    sewarage = scrapy.Field()  # features > sewage
+    heating = scrapy.Field()  # features > heating
+    accessibility = scrapy.Field()  # features > accessibility
+    parking = scrapy.Field()  # features > parking
+    garden = scrapy.Field()  # features > garden
+    risks = scrapy.Field()  # features > risks
+    obligations = scrapy.Field()  # features > obligations
