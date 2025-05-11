4353b77ec77644a523f15f3f4ddf662ef1c02099
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-05-11 16:31:27 -0500

    Refactor items.py: Define PropertyItem, PropertyImageItem, and PropertyLocationItem with appropriate fields for scraped data

--- src/estatesearch/search/search/items.py
+++ src/estatesearch/search/search/items.py
@@ -11,6 +11,58 @@ class SearchItem(scrapy.Item):
     # name = scrapy.Field()
     pass
 
+
 class PropertyItem(scrapy.Item):
-    
+
+    source = scrapy.Field()
+    id = scrapy.Field()
+    transactionType = scrapy.Field()  # transactionType
+    bedrooms = scrapy.Field()
+    bathrooms = scrapy.Field()
+    numberOfImages = scrapy.Field()
+    numberOfFloorplans = scrapy.Field()
+    displayAddress = scrapy.Field()
+    propertyType = scrapy.Field()  # propertySubType
+    summary = scrapy.Field()
+    listingUpdateReason = scrapy.Field()  # listingUpdate > listingUpdateReason
+    price = scrapy.Field()
+    price_frequency = scrapy.Field()
+    price_currencyCode = scrapy.Field()
+    contactTelephone = scrapy.Field() # customer > contactTelephone
+    contact_branchDisplayName = scrapy.Field() # customer > contactBranchDisplayName
+    commercial = scrapy.Field() # commercial
+    development = scrapy.Field() # development
+    residential = scrapy.Field() # residential
+    students = scrapy.Field() # students
+    auction = scrapy.Field() # auction
+    feesApply = scrapy.Field() # feesApply
+    displaySize = scrapy.Field() # displaySize
+    propertyUrl = scrapy.Field()  # propertyUrl
+    firstVisibleDate = scrapy.Field()  # firstVisibleDate
+    propertyTypeFullDescription = scrapy.Field()  # propertyTypeFullDescription
+    isRecent = scrapy.Field()  # isRecent
+
+
+class PropertyImageItem(scrapy.Item):
+    id = scrapy.Field()
+    imageUrl = scrapy.Field()
+    order = scrapy.Field()
+    isPrimary = scrapy.Field()
+    isFloorplan = scrapy.Field()
+    isVideo = scrapy.Field()
+    isVirtualTour = scrapy.Field()
+    isInteractiveFloorplan = scrapy.Field()
+    pass
+
+
+class PropertyLocationItem(scrapy.Item):
+    id = scrapy.Field()
+    latitude = scrapy.Field()
+    longitude = scrapy.Field()
+    displayAddress = scrapy.Field()
+    countryCode = scrapy.Field()
+    countryName = scrapy.Field()
+    regionName = scrapy.Field()
+    districtName = scrapy.Field()
+    subDistrictName = scrapy.Field()
     pass
