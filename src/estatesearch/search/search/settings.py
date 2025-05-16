e4f40c812c5ae2d22d2ed59c4d80498b798ccf0e
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-05-15 21:40:49 -0500

    Refactor settings.py: Adjust download delay and concurrent request settings for improved performance

--- src/estatesearch/search/search/settings.py
+++ src/estatesearch/search/search/settings.py
@@ -25,11 +25,11 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 10
+# DOWNLOAD_DELAY = 3
 RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 1
-CONCURRENT_REQUESTS_PER_IP = 1
+CONCURRENT_REQUESTS_PER_DOMAIN = 8
+CONCURRENT_REQUESTS_PER_IP = 4
 
 # Autothrottle settings
 AUTOTHROTTLE_ENABLED = True
