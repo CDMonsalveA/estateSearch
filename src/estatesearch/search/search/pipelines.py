0b6560c15312211a7ec5e0f50d23531fae9e56bd
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-05-22 19:33:23 -0500

    Refactor SearchPipeline: Add docstrings for methods and improve class documentation for better clarity and maintainability

--- src/estatesearch/search/search/pipelines.py
+++ src/estatesearch/search/search/pipelines.py
@@ -7,8 +7,37 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+from .items import (
+    PropertyImageItem,
+    PropertyInterestPointItem,
+    PropertyItem,
+    PropertyRoomItem,
+)
+
 
 class SearchPipeline:
-    def process_item(self, item, spider):
+    """
+    This pipeline is used to process the scraped items. It is responsible for
+    cleaning, validating, and storing the data. The pipeline can be configured
+    to perform different actions based on the item type.
+    """
 
+    def __init__(self):
+
+        pass
+
+    def open_spider(self, spider):
+        """This method is called when the spider is opened. It can be used to
+        initialize resources or perform setup tasks.
+        """
+        pass
+
+    def close_spider(self, spider):
+        """This method is called when the spider is closed. It can be used to
+        clean up resources or perform final tasks.
+        """
+        pass
+
+    def process_item(self, item, spider):
+        """There are 4 items and each type represemts a table in the database."""
         return item
